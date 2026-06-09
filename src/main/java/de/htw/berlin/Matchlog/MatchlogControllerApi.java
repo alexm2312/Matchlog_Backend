bf4f01e6f5791bbc121b97a4b188dd6b8f0bdb93
Leandro <leandro.schadock@vodafone.de>
2026-06-09 19:08:33 +0200

    ControllerAPI bearbeitet

--- src/main/java/de/htw/berlin/Matchlog/MatchlogControllerApi.java
+++ src/main/java/de/htw/berlin/Matchlog/MatchlogControllerApi.java
@@ -1,12 +1,14 @@
 package de.htw.berlin.Matchlog;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class MatchlogControllerApi {
-    
+
     @GetMapping("/api/matches")
     public List<EntityClass> getHelloWorld() {
         List<EntityClass> matches = new ArrayList<>();
