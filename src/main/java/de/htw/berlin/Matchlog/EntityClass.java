bccf134f1267b8f8461a229f9497be3aa608b226
alexm2312 <alexander.meier@student.htw-berlin.de>
2026-06-24 20:03:16 +0200

    Autorisierung BE 1.1

--- src/main/java/de/htw/berlin/Matchlog/EntityClass.java
+++ src/main/java/de/htw/berlin/Matchlog/EntityClass.java
@@ -15,44 +15,65 @@ public class EntityClass {
     private String title;
     private String location;
     private double ticketprice;
+
+    // PUBLIC oder PRIVATE
     private String visibility;
+
+    // E-Mail des Erstellers
     private String owner;
 
-    public EntityClass() {}
-
-    public EntityClass(String title, String location, double ticketprice, String visibility, String owner) {
-    this.title = title;
-    this.location = location;
-    this.ticketprice = ticketprice;
-    this.visibility = visibility;
-    this.owner = owner;
+    public EntityClass() {
     }
 
-    public Long getId() { return id; }
-    public String getTitle() { return title; }
-    public String getLocation() { return location; }
-    public double getTicketprice() { return ticketprice; }
+    public EntityClass(String title, String location, double ticketprice, String visibility, String owner) {
+        this.title = title;
+        this.location = location;
+        this.ticketprice = ticketprice;
+        this.visibility = visibility;
+        this.owner = owner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public double getTicketprice() {
+        return ticketprice;
+    }
+
+    public String getVisibility() {
+        return visibility;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
 
     public void setTitle(String title) {
         this.title = title;
     }
+
     public void setLocation(String location) {
         this.location = location;
     }
+
     public void setTicketprice(double ticketprice) {
         this.ticketprice = ticketprice;
     }
-    
-    public String getVisibility() {
-        return visibility;
-    }
+
     public void setVisibility(String visibility) {
-        this.visibility = visibility;           
+        this.visibility = visibility;
     }
-    public String getOwner() {
-        return owner;
-    }
+
     public void setOwner(String owner) {
         this.owner = owner;
-    }   
+    }
 }
